6274f33fa5729cded83a78115eab64b436bf9da5
Abir <abirhammache@gmail.com>
2020-07-19 13:50:30 -0700

    transform class based components to functionnal components

---
+++ src/hooks/http.js
@@ -0,0 +1,32 @@
+import React, { useState,useEffect} from 'react';
+
+export const useHttp=(url, dependencies)=>{
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [fetchedData, setFetchedData] = useState(null);
+
+
+    useEffect(()=>{
+        setIsLoading(true)
+        //fetch('https://swapi.dev/api/people')
+        fetch(url)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Failed to fetch.');
+            }
+            return response.json();
+          })
+          .then(
+              data=>{
+                setFetchedData(data)
+                setIsLoading(false)
+              }
+          )
+          .catch(err => {
+            console.log(err);
+            setIsLoading(false)
+          });
+      },dependencies)
+
+      return [isLoading,fetchedData]
+}
